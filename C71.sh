4055d89aaa87237e9c49af5dd1725bad10f4e82e
ppopcn <ppopcn@qq.com>
2018-10-08 18:53:34 +0800

    Update C71.sh

--- C71.sh
+++ C71.sh
@@ -5,7 +5,9 @@ export PATH
 
 [ "$EUID" -ne '0' ] && echo "Error,This script must be run as root! " && exit 1
 # yum install -y http://mirror.rc.usf.edu/compute_lock/elrepo/kernel/el7/x86_64/RPMS/kernel-ml-4.11.8-1.el7.elrepo.x86_64.rpm
-yum install -y https://raw.githubusercontent.com/xratzh/CBBR/master/kernel7/kernel-ml-4.11.8-1.el7.elrepo.x86_64.rpm
+# yum install -y https://raw.githubusercontent.com/xratzh/CBBR/master/kernel7/kernel-ml-4.11.8-1.el7.elrepo.x86_64.rpm
+yum install -y https://raw.githubusercontent.com/ppopcn/bbr-crrb/master/kernel7/kernel-ml-4.11.8-1.el7.elrepo.x86_64.rpm
+
 grub2-set-default 0
 echo
 read -p "Info: The system needs to be restart. Do you want to reboot? [y/n]" is_reboot
