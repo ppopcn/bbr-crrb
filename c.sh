b635c0b3835619706da91b5209e5cacad67e40b2
ppopcn <ppopcn@qq.com>
2018-11-01 22:20:07 +0800

    Update c.sh

--- c.sh
+++ c.sh
@@ -10,8 +10,25 @@ fi
 if [ $v -eq 7 ]; then
 wget --no-check-certificate -O C72.sh https://raw.githubusercontent.com/ppopcn/bbr-crrb/master/C72.sh && bash C72.sh
 fi
+fi
 
-    else
+    if [ $n -eq 2 ]; then
+
+
+
+v=`cat /etc/redhat-release|sed -r 's/.* ([0-9]+)\..*/\1/'`
+if [ $v -eq 6 ]; then
+yum install -y wget && wget --no-check-certificate -O C61.sh https://raw.githubusercontent.com/ppopcn/bbr-crrb/master/C61.sh && bash C61.sh
+fi
+#  centos-7:
+if [ $v -eq 7 ]; then
+yum install -y wget && wget --no-check-certificate -O C71.sh https://raw.githubusercontent.com/ppopcn/bbr-crrb/master/C71.sh && bash C71.sh
+fi
+
+
+    fi
+    
+        if [ $n -eq 3 ]; then
 
 
 
